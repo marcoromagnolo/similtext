2c91a5e7dee408e5743418ae0671cc137936ddf2
Marco Romagnolo <marco@romagnolo.me>
2024-12-09 15:20:34 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 source ./venv/bin/activate
 
 ./venv/bin/python --version
-pip install -r requirements.txt
+./venv/bin/pip install -r requirements.txt
 ./venv/bin/python app.py > ./log/similtext.out 2>&1 &
