826c64659bb31aab2ba039a7ef255124c74a0869
Marco Romagnolo <marco@romagnolo.me>
2024-12-09 16:18:00 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,6 +13,16 @@ LOG_SETTINGS = {
     'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 }
 
+DB_SETTINGS = {
+    'host': 'localhost',
+    'user': 'wordpress',
+    'password': 'password',
+    'database': 'wordpress',
+}
+
+INIT_SCHEDULE_AT = "16:17"
+INIT_FROM_DAYS = 30
+
 STORAGE_DIR = "storage"
 
 OPENAI_SECRET = ""
